891f23c397d1719b44cbda6c2f34cc6a9773056b
dylannkeys <dm096526@gmail.com>
2023-11-23 19:34:37 -0500

    mi primer commit

---
+++ ejemplo.py
@@ -0,0 +1,13 @@
+a="Bienvenidos a mi 'Primer' Programa"
+b='En mi Lenguaje de Programación "Python"'
+
+print (a+" "+b)
+b="con el uso de Python"
+
+print (a,b, "Gracias")
+
+a = 3
+b = 5 
+c = 7 
+
+print(f"El número {a} es el primer valor impar, el segundo es {b} y el tercero es {c}")
